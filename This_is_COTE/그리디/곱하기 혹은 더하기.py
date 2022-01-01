10dcd9e730f4c93436b0f91398659b565cd30f2f
JUNTHEWORLD <junhak1103@naver.com>
2022-01-01 16:19:13 +0900

    Update 곱하기 혹은 더하기.py

--- This_is_COTE/그리디/곱하기 혹은 더하기.py
+++ This_is_COTE/그리디/곱하기 혹은 더하기.py
@@ -12,12 +12,8 @@ print(numbers)
 #result가 1보다 작아도 더해야되네
 #암튼 두 수중에 하나라도 1보다 작으면 더해줘야됨 ex) 1*4 < 1+4 / 5*1 < 5+1
 for num in numbers:
-    if (num == 0 or num == 1):
-        print(num)
+    if (result <= 1 or num <= 1):
         result+=num
     else:
-        if(result == 0):
-            result+=num
-            continue
         result*=num
 print(result)
